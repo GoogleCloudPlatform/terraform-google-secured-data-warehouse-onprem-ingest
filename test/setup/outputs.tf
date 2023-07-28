db54f15e155fb474e4fb0cc968e5a5aba80f10a8
Daniel da Silva Andrade <dandrade@ciandt.com>
2023-07-28 12:24:07 -0300

    fix: add backend_bucket as setup output

--- test/setup/outputs.tf
+++ test/setup/outputs.tf
@@ -18,6 +18,10 @@ output "project_id" {
   value = module.project.project_id
 }
 
+output "backend_bucket" {
+  value = "bkt-ci-tst-${module.project.project_id}"
+}
+
 output "sa_key" {
   value     = google_service_account_key.int_test.private_key
   sensitive = true
